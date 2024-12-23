d4e73a001e99322f629474e67edc9ac487fc6a67
Italo-Chiaradia <italocoelho050704@gmail.com>
2024-12-23 11:45:09 -0300

    add product and benefit sections

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,128 @@
-import { Container, Hero, HeroInnerContainer, SectionProducts, ProductsContainer } from './styles'
+import { Container, Hero, HeroInnerContainer, SectionProducts, ProductsContainer, SectionBenefits, SectionBenefitsContainer } from './styles'
 import { Header } from '../../components/Header'
 import { Product } from '../../components/Product'
 import BgImage from '../../assets/bg-image.png'
 import BgImageDesktop from '../../assets/bg-image-desktop.png'
-import {useMediaQuery} from 'react-responsive'
+import SectionBenefitsImage from '../../assets/section-benefits.png'
+import SectionBenefitsImageDesktop from '../../assets/section-benefits-desktop.png'
+import { useState } from 'react'
+import { useMediaQuery } from 'react-responsive'
+import { CheckCircle  } from '@phosphor-icons/react'
+
+const candles = [
+    {
+        id: 1,
+        title: 'Spiced Mint',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    },
+    {
+        id: 2,
+        title: 'Sweet Strawberry',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    },
+    {
+        id: 3,
+        title: 'Cool Bluberries',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    },
+    {
+        id: 4,
+        title: 'Juicy Lemon',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    },
+    {
+        id: 5,
+        title: 'Fragrant Cinnamon',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    },
+    {
+        id: 6,
+        title: 'Summer Cherries',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    },
+    {
+        id: 7,
+        title: 'Clean Lavander',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    },
+    {
+        id: 8,
+        title: 'Whispering Embers',
+        price: '9.99$',
+        details: {
+            wax: 'Top grade Soy wax that delivers a smoke less, consistent burn',
+            fragrance: 'Premium quality ingredients with natural essentials oils',
+            burningTime: '70-75 hours',
+            dimension: '10cm x 5cm',
+            weight: '400g'
+        }
+
+    }
+]
+
 export function Home() {
+    const [quantityProducts, setQuantityProducts] = useState(4);
     const isDesktop = useMediaQuery({query: '(min-width: 768px)'});
+    function toggleProductsView() {
+        setQuantityProducts(prevState => prevState === candles.length ? candles.length / 2 : candles.length)
+    }
+
     return (
         <Container>
             <Header />
@@ -43,14 +160,76 @@ export function Home() {
                     </p>
                 </header>
                 <ProductsContainer>
-                    <Product />
-                    <Product />
-                    <Product />
-                    <Product />
+                    {
+                        candles.slice(0, quantityProducts).map(candle => {
+                            return (
+                                <Product
+                                    key={candle.title}
+                                    product={candle}
+                                />
+                            )
+                        })
+                    }
 
                 </ProductsContainer>
-                <button>See more</button>
+                    <button onClick={toggleProductsView}>
+                        {quantityProducts === candles.length ? 'See less' : 'See more'}
+                    </button>
             </SectionProducts>
+            <SectionBenefits>
+                <SectionBenefitsContainer>
+                    <div>
+                        <h1>Clean and fragrant soy wax</h1>
+                        <p>Made for your home and for your wellness</p>
+                    </div>
+                    {
+                        !isDesktop?(
+                            <img src={SectionBenefitsImage} />
+                        ) : (
+                            <img src={SectionBenefitsImageDesktop} />
+                        )
+                    }
+                    <div>
+                        <ul>
+                            <li>
+                                <span>
+                                    <CheckCircle />
+                                </span>
+                                <span>
+                                    <strong>Eco-sustainable:</strong> All recyclable materials, 0% CO2 emissions
+                                </span>
+                            </li>
+                            <li>
+                                <span>
+                                    <CheckCircle />
+                                </span>
+                                <span>
+                                    <strong>Hyphoallergenic:</strong> 100% natural, human friendly ingredients 
+                                </span>
+                            </li>
+                            <li>
+                                <span>
+                                    <CheckCircle />
+                                </span>
+                                <span>
+                                    <strong>Handmade:</strong> All candles are craftly made with love.
+                                </span>
+                            </li>
+                            <li>
+                                <span>
+                                    <CheckCircle />
+                                </span>
+                                <span>
+                                    <strong>Long burning:</strong> No more waste. Created for last long.
+                                </span>
+                            </li>
+                        </ul>
+                        <button>
+                            Learn more
+                        </button>
+                    </div>
+                </SectionBenefitsContainer>
+            </SectionBenefits>
         </Container>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-
+    width: 100%;
+    height: 100%;
 `
 
 export const Hero = styled.div`
@@ -96,6 +97,10 @@ export const SectionProducts = styled.section`
             filter: brightness(.8);
         }
     }
+    @media (min-width: 1224px) {
+        max-width: 1111px;
+        margin: 0 auto;
+    }
 `
 
 export const ProductsContainer = styled.div`
@@ -103,5 +108,104 @@ export const ProductsContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 20px;
+    @media (min-width: 1224px) {
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: start;
+    }
+
+`
+export const SectionBenefits = styled.section`
+    width: 100%;
+    padding: 100px 14px;
+    background-color: #F7F8FA;
+
+    @media (min-width: 1224px) {
+        padding-block: 133px;
+    }
+`
+
+export const SectionBenefitsContainer = styled.div`
+    display: grid;
+    grid-template-areas:
+        "header header"
+        "image image"
+        "benefits benefits";
+    grid-row-gap: 64px;
+    > div:first-child {
+        grid-area: header;
+        text-align: center;
+    }
+    > img {
+        grid-area: image;
+        max-width: 100%;
+        height: auto;
+        margin: 0 auto;
+        box-shadow: 0 4px 94px rgba(123, 123, 123, .35);
+    }
+    > div:last-child {
+        width: fit-content;
+        margin: 0 auto;
+        grid-area: benefits;
+        text-align: center;
+    }
+    > div:last-child > button {
+        color: ${({theme}) => theme.white};
+        background-color: ${({theme}) => theme.green};
+        padding: 8px 44px;
+        font-size: 21px;
+        font-weight: 500;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        margin-top: 64px;
+        
+        transition: all .2s;
+        &:hover {
+            filter: brightness(.8);
+        }
+    }
     
+    h1 {
+        font-size: 40px;
+        font-weight: 500;
+        color: #1D252C;
+        margin-bottom: 16px;
+    }
+    p {
+        font-size: 16px;
+        color: ${({theme}) => theme.green};
+    }
+    li {
+        display: flex;
+        gap: 8px;
+    }
+    li svg {
+        margin-top: 4px;
+    }
+    li + li {
+        margin-top: 24px;
+    }
+
+    @media (min-width: 1224px) {
+        max-width: 1111px;
+        margin: 0 auto;
+        grid-template-columns: 540px 540px;
+        grid-template-areas:
+            "header image"
+            "header image"
+            "benefits image";
+        align-items: center;
+        grid-column-gap: 38px;
+        grid-row-gap: 38px;
+        > div:first-child {
+            text-align: left;
+        }
+        > div:last-child {
+            width: 100%;
+            margin: 0;
+            text-align: left;
+        }
+    }
+
 `
